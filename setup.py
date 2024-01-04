2daad1e7bbb39de376b4d4a58f3688a2473ba197
Mplaty <mplatypus54@gmail.com>
2024-01-04 20:43:34 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+def long_description() -> str:
+    with open("README.md") as fp:
+        return fp.read()
+
 setup(
     name='hikari-ongaku',
     version='0.1.0',
     author='MPlaty',
     author_email='mplatypus54@gmail.com',
-    description='A Music handler, for hikari.',
+    description='A Music handler for hikari!',
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     python_requires='>=3.11',
     requires=["hikari", "aiohttp"],
