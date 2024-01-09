9bf88725f5e276cdda7bfd69c608edef88c1140d
Mplaty <mplatypus54@gmail.com>
2024-01-09 19:34:11 +1100

    version 0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description() -> str:
 
 setup(
     name="hikari-ongaku",
-    version="0.1.1",
+    version="0.2",
     author="MPlaty",
     author_email="contact@mplaty.com",
     description="A music/audio handler for hikari!",
