a9fdb5cc3db465fb650d6df5519a9436b9ffa786
Mplaty <mplatypus54@gmail.com>
2024-05-26 20:24:27 +1000

    add tests for abstract class implementations

---
+++ tests/impl/test_events.py
@@ -0,0 +1,82 @@
+# ruff: noqa: D100, D101, D102, D103
+
+from ongaku.impl.errors import ExceptionError
+from ongaku.abc.errors import SeverityType
+from ongaku.impl.events import Ready, PlayerUpdate, TrackException, TrackStuck, WebsocketClosed, TrackStart, TrackEnd
+from ongaku.impl.player import State
+from ongaku.abc.events import TrackEndReasonType
+
+from hikari.snowflakes import Snowflake
+import pytest
+import datetime
+
+from ongaku.impl.track import Track, TrackInfo
+
+@pytest.fixture
+def track() -> Track:
+    track_info = TrackInfo("identifier", False, "author", 1, True, 2, "title", "source_name", "uri", "artwork_url", "isrc")
+    return Track("encoded", track_info, {}, {}, None)
+
+def test_ready():
+    ready = Ready(False, "session_id")
+
+    assert ready.resumed is False
+    assert ready.session_id == "session_id"
+
+
+def test_player_update():
+    time = datetime.datetime.now()
+    state = State(time, 2, False, 3)
+    player_update = PlayerUpdate(Snowflake(1234567890), state)
+
+    assert isinstance(player_update.guild_id, Snowflake)
+    assert player_update.guild_id == Snowflake(1234567890)
+    assert player_update.state == state
+
+
+def test_websocket_closed():
+    websocket_closed = WebsocketClosed(Snowflake(1234567890), 1, "reason", False)
+
+    assert isinstance(websocket_closed.guild_id, Snowflake)
+    assert websocket_closed.guild_id == Snowflake(1234567890)
+    assert websocket_closed.code == 1
+    assert websocket_closed.reason == "reason"
+    assert websocket_closed.by_remote is False
+
+
+
+def test_track_start(track: Track):
+    track_start = TrackStart(Snowflake(1234567890), track)
+
+    assert isinstance(track_start.guild_id, Snowflake)
+    assert track_start.guild_id == Snowflake(1234567890)
+    assert track_start.track == track
+
+
+def test_track_end(track: Track):
+    track_end = TrackEnd(Snowflake(1234567890), track, TrackEndReasonType.FINISHED)
+
+    assert isinstance(track_end.guild_id, Snowflake)
+    assert track_end.guild_id == Snowflake(1234567890)
+    assert track_end.track == track
+    assert track_end.reason == TrackEndReasonType.FINISHED
+
+
+def test_track_exception(track: Track):
+    exception = ExceptionError("message", SeverityType.COMMON, "cause")
+    track_exception = TrackException(Snowflake(1234567890), track, exception)
+
+    assert isinstance(track_exception.guild_id, Snowflake)
+    assert track_exception.guild_id == Snowflake(1234567890)
+    assert track_exception.track == track
+    assert track_exception.exception == exception
+
+
+
+def test_track_stuck(track: Track):
+    track_stuck = TrackStuck(Snowflake(1234567890), track, 1)
+
+    assert isinstance(track_stuck.guild_id, Snowflake)
+    assert track_stuck.guild_id == Snowflake(1234567890)
+    assert track_stuck.track == track
+    assert track_stuck.threshold_ms == 1
---
+++ tests/impl/test_playlist.py
@@ -0,0 +1,31 @@
+# ruff: noqa: D100, D101, D102, D103
+
+import typing
+import pytest
+
+from ongaku.impl.playlist import Playlist, PlaylistInfo
+from ongaku.impl.track import Track, TrackInfo
+
+
+@pytest.fixture
+def track() -> Track:
+    track_info = TrackInfo("identifier", False, "author", 1, True, 2, "title", "source_name", "uri", "artwork_url", "isrc")
+    return Track("encoded", track_info, {}, {}, None)
+
+
+def test_playlist_info():
+    playlist_info = PlaylistInfo("name", 1)
+
+    assert playlist_info.name == "name"
+    assert playlist_info.selected_track == 1
+
+
+def test_playlist(track: Track):
+    info = PlaylistInfo("name", 1)
+    playlist = Playlist(info, [track], {})
+
+    assert playlist.info == info
+    assert playlist.plugin_info == {}
+    assert isinstance(playlist.tracks, typing.Sequence)
+    assert len(playlist.tracks) == 1
+    assert playlist.tracks[0] == track
