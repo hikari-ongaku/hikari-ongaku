7214df42f7cabefca57b125d4d54c52baa5b1626
mplaty <contact@mplaty.com>
2024-08-21 05:38:33 -0700

    Add tests

---
+++ tests/ext/youtube.py
@@ -0,0 +1,78 @@
+import mock
+import pytest
+
+from ongaku import Client
+from ongaku import Session
+from ongaku.ext import youtube as yt
+from ongaku.rest import RESTClient
+
+
+@pytest.mark.asyncio
+async def test_fetch_youtube(ongaku_client: Client, ongaku_session: Session):
+    rest = RESTClient(ongaku_client)
+
+    with (
+        mock.patch.object(rest._client, "_session_handler"),
+        mock.patch.object(
+            rest._client.session_handler,
+            "fetch_session",
+            return_value=ongaku_session,
+        ) as patched_fetch_session,
+        mock.patch.object(
+            ongaku_session,
+            "request",
+            new_callable=mock.AsyncMock,
+            return_value="refresh_token",
+        ) as patched_request,
+    ):
+        assert await yt.fetch_youtube(ongaku_session) == "refresh_token"
+
+        patched_fetch_session.assert_called_once()
+
+        patched_request.assert_called_once_with(
+            "GET",
+            "/youtube",
+            str,
+        )
+
+
+@pytest.mark.asyncio
+async def test_update_youtube(ongaku_client: Client, ongaku_session: Session):
+    rest = RESTClient(ongaku_client)
+
+    # Test with payload
+    with (
+        mock.patch.object(rest._client, "_session_handler"),
+        mock.patch.object(
+            rest._client.session_handler,
+            "fetch_session",
+            return_value=ongaku_session,
+        ) as patched_fetch_session,
+        mock.patch.object(
+            ongaku_session,
+            "request",
+            new_callable=mock.AsyncMock,
+            return_value=None,
+        ) as patched_request,
+    ):
+        await yt.update_youtube(
+            ongaku_session,
+            refresh_token="refresh_token",
+            skip_initialization=False,
+            po_token="po_token",
+            visitor_data="visitor_data",
+        )
+
+        patched_fetch_session.assert_called_once()
+
+        patched_request.assert_called_once_with(
+            "POST",
+            "/youtube",
+            None,
+            json={
+                "refreshToken": "refresh_token",
+                "skipInitialization": False,
+                "poToken": "po_token",
+                "visitorData": "visitor_data",
+            },
+        )
